f777b61392e752a6f3e7587aab52f52aa63c437f
ethanpasta <ethan.mayer9@gmail.com>
2019-05-01 04:29:49 +0000

    Add option for negative digits

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 digit = abs(number) % 10
+if number < 0:
+    digit = digit * (-1)
 print("Last digit of {:d} is {:d} and is".format(number, digit), end = " ")
 if digit > 5:
     print("greater than 5")
