45e28982c5359124c23f7640142d5a11baf0cde2
ethanpasta <ethan.mayer9@gmail.com>
2019-05-01 04:44:02 +0000

    Make more readable without ascii values

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if ord(c) <= 122 and ord(c) >= 97:
+        if ord(c) <= ord('z') and ord(c) >= ord('a'):
             print("{:c}".format(ord(c) - 32), end='')
         else:
             print(c, end='')
