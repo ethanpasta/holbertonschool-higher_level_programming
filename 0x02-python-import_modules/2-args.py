1a96abcf9272cd312cebd089051ed8547eb7bc12
ethanpasta <ethan.mayer9@gmail.com>
2019-05-02 20:55:03 +0000

    Pep8 errors

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,11 +2,11 @@
 import sys
 print("{:d} argument{}{}"
       .format(len(sys.argv) - 1,
-          's' if len(sys.argv) - 1 != 1 else '',
+              's' if len(sys.argv) - 1 != 1 else '',
               '.' if len(sys.argv) - 1 == 0 else ':'))
 if len(sys.argv) - 1 > 0:
-      usr_args = sys.argv[1:]
-      i = 1
-      for arg in usr_args:
-          print("{:d}: {}".format(i, arg))
-          i += 1
+    usr_args = sys.argv[1:]
+    i = 1
+    for arg in usr_args:
+        print("{:d}: {}".format(i, arg))
+        i += 1
