837cf2d10fa5aea14c05fd6d2f7194e1f723f401
ethanpasta <ethan.mayer9@gmail.com>
2019-05-02 19:59:00 +0000

    Task 2 and 3

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+print("{:d} argument{}{}"
+      .format(len(sys.argv) - 1,
+          's' if len(sys.argv) - 1 != 1 else '',
+              '.' if len(sys.argv) - 1 == 0 else ':'))
+if len(sys.argv) - 1 > 0:
+      usr_args = sys.argv[1:]
+      i = 1
+      for arg in usr_args:
+          print("{:d}: {}".format(i, arg))
+          i += 1
