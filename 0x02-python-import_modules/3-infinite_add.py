a3ac7b4f5526dc994f4db3222c4fb51b6d764447
ethanpasta <ethan.mayer9@gmail.com>
2019-05-02 21:25:13 +0000

    Add main check

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import sys
-sum = 0
-for arg in sys.argv[1:]:
-    sum += int(arg)
-print("{:d}".format(sum))
+if __name__ == "__main__":
+    import sys
+    sum = 0
+    for arg in sys.argv[1:]:
+        sum += int(arg)
+        print("{:d}".format(sum))
