25696695847cf592125fe618d2bad07dc9729215
ethanpasta <ethan.mayer9@gmail.com>
2019-05-09 21:05:15 +0000

    Change variable name

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    max = sorted(a_dictionary.values())[0]
+    max_v = max(a_dictionary.values())
     for key in a_dictionary.keys():
-        if a_dictionary[key] == max:
+        if a_dictionary[key] == max_v:
             return key
