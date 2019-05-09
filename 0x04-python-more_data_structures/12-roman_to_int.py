b995fb406b6cd7c92e421e68e1ccebb6447b1a0c
ethanpasta <ethan.mayer9@gmail.com>
2019-05-09 23:41:03 +0000

    New approach

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,19 +2,13 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or not roman_string:
         return None
-    dic_s = {'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
-    dic_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    i = 0
+    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
-    while i < len(roman_string):
+    for i in range(len(roman_string)):
+        first = roman_string[i]
         if i < len(roman_string) - 1:
-            str1 = roman_string[i] + roman_string[i + 1]
+            second = roman_string[i + 1]
+            num += dic[first] if dic[second] <= dic[first] else -dic[first]
         else:
-            str1 = roman_string[i]
-        if str1 in dic_s.keys():
-            num += dic_s[str1]
-            i += 2
-        else:
-            num += dic_n[roman_string[i]]
-            i += 1
+            num += dic[first]
     return num
