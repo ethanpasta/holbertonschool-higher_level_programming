037f9e64c26bad0b84bc9b7495300b7e90909d78
ethanpasta <ethan.mayer9@gmail.com>
2019-05-20 20:52:57 +0000

    Remove newline from stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except ValueError as e:
-        sys.stderr.write("Exception: " + str(e) + "\n")
+        sys.stderr.write("Exception: " + str(e))
         return False
