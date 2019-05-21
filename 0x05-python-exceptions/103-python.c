4d6bae721ab20c7aac37ef2cafe5e6aa0018bdb0
ethanpasta <ethan.mayer9@gmail.com>
2019-05-21 03:04:26 +0000

    Advanced task 10

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,89 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+#include <float.h>
+
+void print_python_bytes(PyObject *p);
+void print_python_list(PyObject *p);
+void print_python_float(PyObject *p);
+
+/**
+ * print_python_list - function prints info about a python list
+ * @p: PyObject
+ *
+ * Return: none
+ */
+void print_python_list(PyObject *p)
+{
+	size_t len, i;
+	PyListObject *pl = (PyListObject *)p;
+
+	printf("[*] Python list info\n");
+	if (!PyList_CheckExact(p))
+	{
+		printf("  [ERROR] Invalid List Object\n");
+		return;
+	}
+	len = PyList_Size(p);
+	printf("[*] Size of the Python List = %lu\n", len);
+	printf("[*] Allocated = %lu\n", pl->allocated);
+	for (i = 0; i < len; i++)
+	{
+		printf("Element %lu: %s\n", i, pl->ob_item[i]->ob_type->tp_name);
+		if (PyBytes_Check((PyObject *)pl->ob_item[i]))
+			print_python_bytes((PyObject *)pl->ob_item[i]);
+		else if (PyFloat_Check((PyObject *)pl->ob_item[i]))
+			print_python_float((PyObject *)pl->ob_item[i]);
+	}
+}
+
+/**
+ * print_python_bytes - function prints info about a Python Bytes object
+ * @p: PyObject
+ *
+ * Return: none
+ */
+void print_python_bytes(PyObject *p)
+{
+	int check = PyBytes_Check(p);
+	char *str;
+	unsigned long int i, size;
+
+	printf("[.] bytes object info\n");
+	if (!check)
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+	size = PyBytes_Size(p);
+	printf("  size: %lu\n", size);
+	str = PyBytes_AsString(p);
+	printf("  trying string: %s\n", str);
+	printf("  first %lu bytes:", (size <= 10) ? size + 1 : 10);
+	for (i = 0; i < size + 1 && i < 10; i++)
+	{
+		printf(" %02x", str[i] & 0xff);
+	}
+	printf("\n");
+}
+
+/**
+ * print_python_float - function prints info about a Python Float Object
+ * @p: PyObject
+ */
+void print_python_float(PyObject *p)
+{
+	double val;
+
+	printf("[.] float object info\n");
+	if (!PyFloat_Check(p))
+	{
+		printf("  [ERROR] Invalid Float Object\n");
+		return;
+	}
+	val = PyFloat_AsDouble(p);
+	if ((long int)val - val == 0)
+		printf("  value: %.1f\n", val);
+	else
+		printf("  value: %.16g\n", val);
+}
