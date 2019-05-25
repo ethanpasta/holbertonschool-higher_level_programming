7c5d8c3ca4e070b8d176be280fe7eacf425fe27f
ethanpasta <ethan.mayer9@gmail.com>
2019-05-25 06:55:04 +0000

    pep8

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -5,7 +5,7 @@ def matrix_mul(m_a, m_b):
     if type(m_b) is not list:
         raise TypeError('m_b must be a list')
     if m_a == [] or m_a == [[]]:
-         raise ValueError('m_a can\'t be empty')
+        raise ValueError('m_a can\'t be empty')
     if m_b == [] or m_b == [[]]:
         raise ValueError('m_b can\'t be empty')
     row_l = len(m_a[0])
