b9638d92b5faf356a5b7ff730668b948df07c988
ethanpasta <ethan.mayer9@gmail.com>
2019-05-28 20:13:14 +0000

    Tasks 0-9

---
+++ 0x08-python-more_classes/8-rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+class Rectangle:
+    number_of_instances = 0
+    print_symbol = '#'
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        return (2 * self.__height) + (2 * self.__width)
+
+    def __str__(self):
+        my_str = ''.join(
+            ['\n' if i % (self.__width + 1) == 0 else str(self.print_symbol)
+             for i in range(1, self.__width * self.__height + self.__height)])
+        return my_str
+
+    def __repr__(self):
+        my_str = "Rectangle(" + str(
+            self.__width) + "," + str(self.__height) + ")"
+        return my_str
+
+    def __del__(self):
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError('rect_1 must be an instance of Rectangle')
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError('rect_2 must be an instance of Rectangle')
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        else:
+            return rect_2
