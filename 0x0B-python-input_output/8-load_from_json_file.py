6475a1550a9ed602e8e212f84efb63da2650ac53
ethanpasta <ethan.mayer9@gmail.com>
2019-06-06 23:57:15 +0000

    Change loads to load

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import json
 
+
 def load_from_json_file(filename):
     with open(filename) as f:
-        obj = json.loads(f.read())
-        return obj
+        return json.load(f)
