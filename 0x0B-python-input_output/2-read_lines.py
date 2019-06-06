a5209eb68b99049a96f4e08eacc2b4c5c2b262ad
ethanpasta <ethan.mayer9@gmail.com>
2019-06-06 23:45:54 +0000

    Add seek statement

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -4,6 +4,7 @@ def read_lines(filename="", nb_lines=0):
     with open(filename) as f:
         for line in f:
             sum_l += 1
+    f.seek(0)
     if nb_lines <= 0 or nb_lines >= sum_l:
         nb_lines = sum_l
     with open(filename) as f:
