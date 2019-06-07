3588728c7dfa2503b6c87eb511bee7f4d1dee324
ethanpasta <ethan.mayer9@gmail.com>
2019-06-07 00:12:56 +0000

    Documentation

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -1,14 +1,28 @@
 #!/usr/bin/python3
+"""Module 11-student"""
+
+
 class Student:
+    """Custom class Student"""
+
     def __init__(self, first_name, last_name, age):
+        """Initialization function
+
+        Args:
+            first_name: first name of student
+            last_name: last name of student
+            age: age of student
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    def to_json(self, attrs=None):
-        my_dic = {}
-        if list and all(isinstance(s, str) for s in attrs):
-            for x in attrs:
-                if x in self.__dict__:
-                    my_dic[x] = self.__dict__[x]
+
+    def to_json(self):
+        """Returns dictionary representation of a student instance"""
+         my_dic = {}
+         if list and all(isinstance(s, str) for s in attrs):
+             for x in attrs:
+                 if x in self.__dict__:
+                     my_dic[x] = self.__dict__[x]
             return my_dic
         return self.__dict__
