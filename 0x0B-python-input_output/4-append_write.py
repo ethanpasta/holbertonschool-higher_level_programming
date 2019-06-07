fd1990255c5e5fea59584131c833939a6d0577ab
ethanpasta <ethan.mayer9@gmail.com>
2019-06-07 00:55:35 +0000

    Documentation

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""Module 4-append_write"""
+
+
 def append_write(filename="", text=""):
+    """Appends a string to the end of a text file
+    Returns number of characters added
+    """
     with open(filename, 'a+') as f:
         return f.write(text)
