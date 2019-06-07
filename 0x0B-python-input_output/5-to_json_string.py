a99b1af58edb015f9f5d5eb4b5fb23b83b85bd14
ethanpasta <ethan.mayer9@gmail.com>
2019-06-07 00:56:10 +0000

    Documentation

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Module 5-to_json_string"""
 import json
 
 
 def to_json_string(my_obj):
+    """Returns json representation of an object"""
     j = json.dumps(my_obj)
     return j
