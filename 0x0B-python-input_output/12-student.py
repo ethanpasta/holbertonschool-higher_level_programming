d48f878450a0a68ed99f8001374b5effcd02a07a
ethanpasta <ethan.mayer9@gmail.com>
2019-06-07 02:08:48 +0000

    Add parameter in to_json method

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -17,12 +17,12 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """Returns dictionary representation of a student instance"""
-         my_dic = {}
-         if list and all(isinstance(s, str) for s in attrs):
-             for x in attrs:
-                 if x in self.__dict__:
-                     my_dic[x] = self.__dict__[x]
+        my_dic = {}
+        if list and all(isinstance(s, str) for s in attrs):
+            for x in attrs:
+                if x in self.__dict__:
+                    my_dic[x] = self.__dict__[x]
             return my_dic
         return self.__dict__
