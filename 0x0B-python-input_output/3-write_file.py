c46755121165d7f3bd490d8584681a26557b3350
ethanpasta <ethan.mayer9@gmail.com>
2019-06-07 00:54:54 +0000

    Documentation

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""Module 3-write_file"""
+
+
 def write_file(filename="", text=""):
+    """Writes a string to a text file and returns number
+    of characters written"""
     with open(filename, 'w+') as f:
         return f.write(text)
