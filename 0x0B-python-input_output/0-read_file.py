0e32b01cb9b5bab4b4d6ba3804a23650f542b604
ethanpasta <ethan.mayer9@gmail.com>
2019-06-07 00:53:16 +0000

    Documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Module 0-read_file"""
+
+
 def read_file(filename=""):
+    """Reads a text file and prints to stdout"""
     with open(filename) as f:
         for line in f:
             print(line, end="")
--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Module 1-number_of_lines"""
+
+
 def number_of_lines(filename=""):
+    """Returns number of lines of a text file"""
     with open(filename) as f:
         num_l = 0
         for line in f:
