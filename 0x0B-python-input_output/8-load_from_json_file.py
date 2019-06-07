0a358ca7b020cb081244913a9d5814a230741de4
ethanpasta <ethan.mayer9@gmail.com>
2019-06-07 00:58:06 +0000

    Documentation

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Module 8-load_from_json_file"""
 import json
 
 
 def load_from_json_file(filename):
+    """Function creates an object from a json file"""
     with open(filename) as f:
         return json.load(f)
