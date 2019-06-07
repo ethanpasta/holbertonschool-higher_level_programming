511bf56fb38839447123160f46eee0a28745d366
ethanpasta <ethan.mayer9@gmail.com>
2019-06-07 00:56:46 +0000

    Documentation

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Module 5-from_json_string"""
 import json
 
 
 def from_json_string(my_str):
+    """Returns object represented by a json string"""
     j = json.loads(my_str)
     return j
