a20e8fbfc11f93e8a0dae3dd56dbed5f4e6e8409
ethanpasta <ethan.mayer9@gmail.com>
2019-07-17 05:00:31 +0000

    Change inner join to left join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,5 +2,5 @@
 -- Script lists all cities in database `hbtn_0d_usa`
 SELECT cities.id, cities.name, states.name
 FROM cities
-INNER JOIN states ON states.id = cities.state_id
+LEFT JOIN states ON states.id = cities.state_id
 ORDER BY cities.id DESC;
