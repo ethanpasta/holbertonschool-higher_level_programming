8e40108b6f0b8095cd2dc5a5f84624e2f3aec7a0
ethanpasta <ethan.mayer9@gmail.com>
2019-07-17 05:42:00 +0000

    Task 13

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,5 @@
+-- Task 14
+-- Script lists all genres of the show Dexter
+SELECT tv_genres.name FROM tv_genres, tv_shows, tv_show_genres
+WHERE tv_shows.title = "Dexter" AND tv_shows.id = tv_show_genres.show_id AND tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_genres.name;
