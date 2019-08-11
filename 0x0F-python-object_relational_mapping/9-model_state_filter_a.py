175710efdf0abc1af9a9c9182fe6ceb17e6b341e
ethanpasta <ethan.mayer9@gmail.com>
2019-08-11 17:27:34 +0000

    Add __name__ == __main__

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 """ Module for task 9 """
 
-import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
+if __name__ == "__main__":
 
-engine = create_engine(
-    'mysql+mysqldb://{}:{}@localhost:3306/{}'
-    .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-Base.metadata.create_all(engine)
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
 
-Session = sessionmaker(bind=engine)
-session = Session()
-states = session.query(State).order_by(State.id).all()
-print("\n".join(["{}: {}"
-                 .format(state.id, state.name)
-                 for state in states if 'a' in state.name]))
-session.close()
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id).all()
+    print("\n".join(["{}: {}"
+                     .format(state.id, state.name)
+                     for state in states if 'a' in state.name]))
+    session.close()
