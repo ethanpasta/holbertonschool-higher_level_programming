cd29574584f76a15d31b0d8f63b5ac65071eea67
ethanpasta <ethan.mayer9@gmail.com>
2019-08-11 17:23:39 +0000

    Change to .first()

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Module for task 8 """
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine(
+    'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+states = session.query(State).order_by(State.id).first()
+if states:
+    print("{}: {}".format(states.id, states.name))
+else:
+    print("Nothing")
+session.close()
