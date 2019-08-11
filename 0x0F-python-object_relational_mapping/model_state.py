2b5a512a2d82371fbde725223e89783de7810e35
ethanpasta <ethan.mayer9@gmail.com>
2019-08-11 02:05:35 +0000

    Task 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module for first orm"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """ Custom class for state object """
+    __tablename__ = 'states'
+    id = Column(Integer, nullable=False, primary_key=True, unique=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
