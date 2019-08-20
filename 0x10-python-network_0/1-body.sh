56649ddb0f12fbbbd4e5821da19b1845305a2f9e
ethanpasta <ethan.mayer9@gmail.com>
2019-08-20 20:04:08 +0000

    Shellcheck syntax

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s --location $1
+curl -s --location "$1"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script sends a post request with data
-curl --data "email=hr@holbertonschool.com&subject=I will always be here for PLD" -s $1
+curl --data "email=hr@holbertonschool.com&subject=I will always be here for PLD" -s "$1"
