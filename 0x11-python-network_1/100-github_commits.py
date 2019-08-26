0a823447818abca233e0b9fdf284bc03f58c9a0e
ethanpasta <ethan.mayer9@gmail.com>
2019-08-26 20:44:39 +0000

    Change url

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
 """ Module for task 100 """
 
-
-def parse(url, num):
-    r = requests.get(url)
-    data = r.json()
-    for t in data:
-        print("{}: {}".format(t.get('commit').get('tree').get('sha'),
-                              t.get('commit').get('committer').get('name')))
-    if 'next' in r.links.keys() and num == 0:
-        parse(r.links.get('next').get('url'), 1)
-
-
 if __name__ == "__main__":
 
     import requests
     import sys
 
-    parse("https://api.github.com/repos/{}/{}/commits?per_page=5&page=1"
-          .format(sys.argv[2], sys.argv[1]), 0)
+    r = requests.get("https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1]))
+    data = r.json()
+    for t in data[0:10]:
+        print("{}: {}".format(t.get('commit').get('tree').get('sha'),
+                              t.get('commit').get('committer').get('name')))
