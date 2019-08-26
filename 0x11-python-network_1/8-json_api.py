31012698820e98ef08fcfd391fa4b2129827bc6f
ethanpasta <ethan.mayer9@gmail.com>
2019-08-26 18:25:24 +0000

    Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module for task 8 """
+
+import requests
+import sys
+
+let = ""
+if len(sys.argv) >= 2:
+    let = sys.argv[1]
+resp = requests.post('http://0.0.0.0:5000/search_user', data={'q': let})
+try:
+    j = resp.json()
+    if j == {}:
+        print("No result")
+    else:
+        print("[{}] {}".format(j['id'], j['name']))
+except ValueError:
+    print("Not a valid JSON")
