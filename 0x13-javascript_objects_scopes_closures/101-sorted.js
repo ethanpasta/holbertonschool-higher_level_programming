89a905275d586cbca2a111f4a661666032196127
ethanpasta <ethan.mayer9@gmail.com>
2019-09-23 19:44:13 +0000

    Advanced task 12

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const dict = require('./101-data.js').dict;
+const newdict = {};
+for (var key in dict) {
+  const val = dict[key];
+  if (newdict[val] === undefined) {
+    newdict[val] = [key];
+  } else {
+    newdict[val].push(key);
+  }
+}
+console.log(newdict);
