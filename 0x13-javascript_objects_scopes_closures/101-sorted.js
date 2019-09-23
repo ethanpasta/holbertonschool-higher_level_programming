f2d5ffcce8a20af3f4e749842e669e4ee3ad4865
ethanpasta <ethan.mayer9@gmail.com>
2019-09-23 20:34:15 +0000

    Change var to const

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
 const newdict = {};
-for (var key in dict) {
+for (const key in dict) {
   const val = dict[key];
   if (newdict[val] === undefined) {
     newdict[val] = [key];
