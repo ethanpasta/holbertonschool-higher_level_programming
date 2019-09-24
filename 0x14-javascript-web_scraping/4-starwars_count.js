dc93a093c05d49b62a446301a64e0a81ec4135a2
ethanpasta <ethan.mayer9@gmail.com>
2019-09-24 21:06:12 +0000

    Use includes instead

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,10 +4,9 @@ request(process.argv[2], function (error, response, body) {
   if (error) console.log(error);
   const info = JSON.parse(body);
   let count = 0;
-  const character = 'https://swapi.co/api/people/18/';
   for (let i = 0; i < info.results.length; i++) {
     for (let j = 0; j < info.results[i].characters.length; j++) {
-      if (info.results[i].characters[j] === character) {
+      if (info.results[i].characters[j].includes('/18/')) {
         count++;
       }
     }
