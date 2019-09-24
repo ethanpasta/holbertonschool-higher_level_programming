2ab70626a5d5a43486e5bfe35f8c49e8fd4832bd
ethanpasta <ethan.mayer9@gmail.com>
2019-09-24 22:02:12 +0000

    Advanced task 8

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const request = require('request');
+
+function printPeople (people, index) {
+  if (index >= people.index) {
+    return;
+  }
+  request(people[index], function (err, resp, body) {
+    if (err) console.log(err);
+    else {
+      console.log(JSON.parse(body).name);
+      return printPeople(people, index + 1);
+    }
+  });
+}
+
+request('https://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
+  if (error) console.log(error);
+  const film = JSON.parse(body);
+  const characters = film.characters;
+  printPeople(characters, 0);
+});
