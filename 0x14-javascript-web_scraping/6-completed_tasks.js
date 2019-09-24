96d832421538da8154b3dea4ef3ae677906fe52a
ethanpasta <ethan.mayer9@gmail.com>
2019-09-24 18:11:12 +0000

    Task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (error) console.log(error);
+  const info = JSON.parse(body);
+  const dict = {};
+  for (let i = 0; i < info.length; i++) {
+    if (dict[info[i].userId] === undefined) {
+      dict[info[i].userId] = 1;
+    } else {
+      dict[info[i].userId]++;
+    }
+  }
+  console.log(dict);
+});
