dbeb85472472fe9c9b78bd0fd1756a858ff3215e
ethanpasta <ethan.mayer9@gmail.com>
2019-09-24 20:29:12 +0000

    Update- only completed tasks

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,9 +5,9 @@ request(process.argv[2], function (error, response, body) {
   const info = JSON.parse(body);
   const dict = {};
   for (let i = 0; i < info.length; i++) {
-    if (dict[info[i].userId] === undefined) {
+    if (dict[info[i].userId] === undefined && info[i].completed) {
       dict[info[i].userId] = 1;
-    } else {
+    } else if (info[i].completed) {
       dict[info[i].userId]++;
     }
   }
